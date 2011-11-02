e1e7a0521f3608e8ceee138a50f6f3ef2684cfc9
Jonas Gehring <jonas.gehring@inovex.de>
2011-11-02 14:52:41 +0100

    list mit beispiel eintraegen

--- src/com/example/googletasks/activities/TasksActivity.java
+++ src/com/example/googletasks/activities/TasksActivity.java
@@ -1,20 +1,49 @@
 package com.example.googletasks.activities;
 
 import android.app.ListActivity;
+import android.content.AsyncQueryHandler;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 import com.example.googletasks.R;
+import com.example.googletasks.content.TasksContentProvider;
 
 public class TasksActivity extends ListActivity {
+	private CursorAdapter cursorAdapter;
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.tasks);
+
+		// setup list
+		ListView list = getListView();
+		list.setAdapter(cursorAdapter = new SimpleCursorAdapter(getApplicationContext(), R.layout.list_tasks_item, null, new String[] {
+			"NAME"
+			, "NOTES"
+			, "DUE_DATE"
+		}, new int[] {
+			R.id.list_tasks_item_name
+			, R.id.list_tasks_item_notes
+			, R.id.list_tasks_item_due_date
+		}));
+
+		// load cursor
+		AsyncQueryHandler query = new AsyncQueryHandler(getContentResolver()) {
+			@Override
+			protected void onQueryComplete(int token, Object cookie, Cursor cursor) {
+				cursorAdapter.changeCursor(cursor);
+			}
+		};
+		query.startQuery(0, 0, TasksContentProvider.CONTENT_URI, null, null, null, null);
 	}
 
 	@Override
---
+++ src/com/example/googletasks/content/TasksContentProvider.java
@@ -0,0 +1,59 @@
+package com.example.googletasks.content;
+
+import java.util.Date;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.net.Uri;
+
+public class TasksContentProvider extends ContentProvider {
+	public static final Uri CONTENT_URI = Uri.parse("content://com.example.googletasks.contentprovider");
+
+	@Override
+	public int delete(Uri uri, String selection, String[] selectionArgs) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getType(Uri uri) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Uri insert(Uri uri, ContentValues values) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean onCreate() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+		MatrixCursor cursor = new MatrixCursor(new String[] {
+				"_ID", "NAME", "DONE", "DUE_DATE", "NOTES"
+		}, 2);
+		cursor.addRow(new Object[] {
+				0, "Auto waschen", false, null, "maximal 10 €"
+		});
+		cursor.addRow(new Object[] {
+				1, "Postbank kündigen", true, new Date(), null
+		});
+		return cursor;
+	}
+
+	@Override
+	public int update(Uri uri, ContentValues values, String selection,
+			String[] selectionArgs) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
